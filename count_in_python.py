f856515ab73e3d94ef6927bf7dcb8a94da8fe060
Unalterable <diamond.oliver@gmail.com>
2016-07-29 13:18:14 +0100

    comparisons

---
+++ count_in_python.py
@@ -0,0 +1,10 @@
+import time
+
+start = time.time()
+
+for i in range(0, 1000000000):
+  pass
+
+end = time.time()
+
+print(end-start)
---
+++ count_in_ruby.rb
@@ -0,0 +1,7 @@
+
+start = Time.now
+(0...1000_000_000).each do |i|
+
+end
+finish = Time.now
+puts finish - start
